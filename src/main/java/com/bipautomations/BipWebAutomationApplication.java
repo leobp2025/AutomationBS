415c663d71fa7ee7f017f7f94a3f97ff6bb1eecf
leobp2025 <lcespedes@bpba.com.ar>
2026-08-20 14:56:14 -0300

    Add main Spring Boot application class

---
+++ src/main/java/com/bipautomations/BipWebAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.bipautomations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipWebAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BipWebAutomationApplication.class, args);
+    }
+
+}
